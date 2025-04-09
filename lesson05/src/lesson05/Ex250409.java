f2f91013e259b43053691b8f19e571909528e015
pollimansa <pollimansa@gmail.com>
2025-04-09 23:54:13 +0900

    과제작성함

--- lesson05/src/lesson05/Ex250409.java
+++ lesson05/src/lesson05/Ex250409.java
@@ -1,5 +1,4 @@
 package lesson05;
-
 import java.util.Scanner;
 
 public class Ex250409 {
@@ -9,19 +8,18 @@ public class Ex250409 {
 		// 그 아이디가 admin, 비밀번호가 1234일때 로그인 성공
 		// 단, 아이디가 admin , 비밀번호 틀렸을때 로그인 실패 > 잘못된 비밀번호 출력
 		// 단2, 아이디가 admin이 아니면 없는 계정 메시지 출력
-		
 		String admin = "admin";
 		String password = "1234";
 		System.out.println("[1문] 로그인 인증하기");
-		System.out.println("아이디를 입력 > ");
+		System.out.print("아이디를 입력 > ");
 		Scanner scanAdmin = new Scanner (System.in);
 		String inputAdmin = scanAdmin.nextLine();
-		System.out.println("비밀번호를 입력 > ");
+		System.out.print("비밀번호를 입력 > ");
 		Scanner scanPassword = new Scanner (System.in);
 		String inputPassword = scanPassword.nextLine();
 		//사용자의 입력을 문자열로 만들어 주는 변수가 반드시 필요한가?
 		//바로 scanAdmin, scanPassword에 대입시키면 안되는건가?
-		//ㄴ 안됨.. 입력받은거 문자취급안해줘서 비교를 못함..
+		//없으면 안됨.. 입력받은거 문자취급안해줘서 비교를 못함..
 		
 		if(admin.equals(inputAdmin)) {
 			if(password.equals(inputPassword)) {
@@ -29,8 +27,8 @@ public class Ex250409 {
 			}
 			else {
 				System.out.println("비밀번호가 틀렸습니다");
-				}
 			}
+		}
 		else {
 		System.out.println("존재하지 않는 계정입니다");
 		}
@@ -45,39 +43,61 @@ public class Ex250409 {
 		//입력받은 숫자 % 3이 0인경우 3의 배수.
 		//입력받은 숫자 % 2가 0이어야 짝수.
 	
-		System.out.println("[2문] 입력한 숫자가 3의 배수인지 짝수인지 알려줍니다. >");				
+		System.out.print("[2문] 입력한 숫자가 3의 배수인지 짝수인지 알려줍니다. >");				
 		//입력받은 숫자 담아둘 변수 하나 만들기
 		Scanner scanNumthr = new Scanner (System.in);
-		//스캔으로 받은 string 어떻게 int형으로 바꾸지..
-
-		int scanNumthr = Scanner.nexInt() ;
+		int numthr = scanNumthr.nextInt();
+		//nextInt : 입력값을 인트로 저장
+		//nextLine : 스트링으로 저장		
 		
-		if (numthr % 3 == 0) {
-			System.out.println("3의 배수입니다.");
-			if (numthr % 2 == 0) {
-				System.out.println("3의 배수이자 짝수입니다.");
-			}
+		//*** 만약 음수의 경우에는? 0의 경우에는? 예외처리 해줘야 함.
+		if (numthr > 0) {
+			if (numthr % 3 == 0) {
+				if (numthr % 2 == 0) {
+					System.out.println("3의 배수이자 짝수입니다.");
+				}
+				else {
+					System.out.println("3의 배수입니다.");
+				}
+			} //3의 배수인가? 짝수인가?
+			else if (numthr % 2 == 0) {
+				System.out.println("짝수입니다.");
+			}//3의 배수가 아님. 그럼 짝수인가?
+				else {
+					System.out.println("3의 배수도 아니고 짝수도 아닙니다.");
+				}//3의 배수도 짝수도 아님. 문구 출력
 		}
-			else if {
-				
-		if (numthr % 2 == 0) {
-			System.out.println("짝수입니다.");
-			
+		else {
+			System.out.println("잘못된 입력입니다.");
 		}
-				
-			}
-		}
-		
-		System.out.println("3의 배수도 아니고 짝수도 아닙니다.");
-		
-		
+		System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
 		//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-		//3번 문제 :
-		// 월(month)을 입력 받아서 계절을 출력
+		//3번 문제 : 월(month)을 입력 받아서 계절을 출력
 		// 봄(3 4 5), 여름(6 7 8) ... 3개월씩
+		System.out.print("[3문] 입력한 숫자에 맞는 계절을 알려줍니다 >");				
+		Scanner scanSeason = new Scanner (System.in);
+		int season = scanSeason.nextInt();
 		
-		
-		}
-	}
-
-
+		if (season > 0 && season < 13) {
+			switch (season) {
+				case 3 : case 4 : case 5 :
+					System.out.println(season+"월은 봄입니다.");
+					break ;
+				case 6 : case 7 : case 8 :
+					System.out.println(season+"월은 여름입니다.");
+					break ;
+				case 9 : case 10 : case 11 :
+					System.out.println(season+"월은 가을입니다.");
+					break ;
+				case 1 : case 2 : case 12 :
+					System.out.println(season+"월은 겨울입니다.");
+					break ;
+			}
+		} 
+		else {
+			System.out.println("잘못된 입력입니다.");
+		} 
+		// if 아래에 스위치랑 case 써도 되나..? 에러 안났으니까 된거같음..
+		// 잘못된 입력 범위 설정하기에 가장 편한건 if 같아서 사용해봤습니다.
+	}		
+}
--- lesson05/src/lesson05/Ex250409_2.java
+++ lesson05/src/lesson05/Ex250409_2.java
@@ -2,35 +2,36 @@ package lesson05;
 
 public class Ex250409_2 {
 	public static void main(String[] args) {
-		// 교재 연습문제 (p.79~80)
-		
+		// 교재 연습문제 (p.79~80)		
 	System.out.println("[1문] 한반에 27명. 5다스를 모두에게 나눠주면 몇개가 남을까");
 	
-	int colorPen = 5 * 12 ;
-	int studentCount = 27 ;
+	int colorPen = 5 * 12 ; // 나눠 줄 펜의 개수
+	int studentCount = 27 ; // 한 반에 있는 학생 수
 	
 	int divColorPen = colorPen / studentCount ;
 	System.out.println("학생당 나눠가지는 색연필 수 : " + divColorPen );
 	//학생들에게 펜을 나눠주면 각자 갖게되는 색연필의 갯수
-	
 	int remainColorPen = colorPen % studentCount ;
 	System.out.println("똑같이 나눠가지고 남은 색연필 수 : " + remainColorPen );
 	//전부 나눠주고 남은 색연필의 갯수
-
 	System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
 	//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-	
 	System.out.println("[2문] 놀이기구에 탈 수 있을까");
-	
+
+	// 타려는 사람의 스펙
 	int age = 4 ;
 	int height = 120 ;
-	int parent = 1 ; // 1 : 부모동반, 0 : 부모동반하지않음
-	int hearchDease = 1 ; // 1 : 심장질환없음, 0 : 심장질환있음 
+
+	boolean parent = false ; // T : 부모동반, F : 부모동반하지않음
+	boolean hearchDease = false ; // T : 심장질환없음, F : 심장질환있음
+	//parent랑 hearchDease를 int 1,0로 처리했는데 
+	//bool이랑 int랑 용량이 4배 차이 나는군요..?
+	//코딩할 때 true랑 false를 T,F처럼 더 줄여서 쓸수있는 방법은 없나요?
 	
 	boolean noru = false ;
 		//기본값 = 탑승불가
 	
-	if (hearchDease == 1) {
+	if (hearchDease) {
 		//심장질환 없다면 참
 		if (height >= 120) {
 			//키120 넘는다면 참
@@ -38,21 +39,19 @@ public class Ex250409_2 {
 				//6세이상이면 참
 				noru = true;
 			}
-			else if (parent == 1) {
+			else if (parent) {
 				//6세이상은 아니지만 부모동반함
 				noru = true ;
 			}
 		}	
-	
-	System.out.println(noru);
-	//이거 문자열로 바꾸는거 어케하더라.. 일단 다른 문제 풀기~
 	}
+	System.out.println(noru == true ? "탑승가능":"탑승불가");
 	System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
 	//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-	
 	System.out.println("[3문] 연도를 입력하면 윤년인지 아닌지 판단");
 	
 	int year = 2020;
+	
 //	boolean leapYear  ;
 	
 	//윤년이면 true, 윤년이 아니면 false
@@ -61,9 +60,31 @@ public class Ex250409_2 {
 	System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
 	//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 	System.out.println("[4문] 가게에서 물건 구매");
+/*	
+	int price = 187000 ;
 	
-	int 
+	int oman = price 
+	int ilman = 
+	int ochun = 
+	int ilchun = 
 	
+	//아 이거 쌤이 십진수를 이진수로 바꿀때 알려준 방법이 있었는데
+	// 그 방법을 쓰면 정말 좋을 것 같은데!!! 그 방법이!!
+	// 뭐였을까요....... 가장 큰것부터 나누는거였던가.......
+	System.out.println(price+"원을 가장 적은 지폐수량으로 지불하려면,");
+	System.out.println("5만원권 : " + oman + "장");
+	System.out.println("1만원권 : " + ilman + "장");
+	System.out.println("5천원권 : " + ochun + "장");
+	System.out.println("1천원권 : " + ilchun + "장");
 	
+*/
+	System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+	System.out.println("[5문] 십의자리 이하를 버리고 0으로 처리하는 코드");
+
+	int number = 235 ;
+	int result = number / 100 * 100 ;
+	
+	System.out.println(result);	
 	}
 }
